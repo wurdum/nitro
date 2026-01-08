44f593b8cc96b7aaafff413665151589fb61b027
Pavlo Rytikov <pavlo@nethermind.io>
2026-01-08 19:10:15 +0200

    Return magic +Gas(1)

--- arbitrator/wasm-libraries/user-host-trait/src/lib.rs
+++ arbitrator/wasm-libraries/user-host-trait/src/lib.rs
@@ -231,7 +231,7 @@ pub trait UserHost<DR: DataReader>: GasMeteredMachine {
         let gas_left = self.gas_left()?;
         let (gas_cost, outcome) = self
             .evm_api()
-            .flush_storage_cache(clear, gas_left)?;
+            .flush_storage_cache(clear, gas_left + Gas(1))?;
         if self.evm_data().arbos_version >= ARBOS_VERSION_STYLUS_CHARGING_FIXES {
             self.buy_gas(gas_cost)?;
         }
